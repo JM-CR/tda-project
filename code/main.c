13c708aeee2edfac7b632c7b93830bc7a550eb4e
IvanGF199 <39014961+IvanGF199@users.noreply.github.com>
2020-05-13 23:17:25 -0500

    Update main.c

--- code/main.c
+++ code/main.c
@@ -1,5 +1,5 @@
 // File: main.c
-// Date: 10/05/20
+// Date: 13/05/20
 
 // ------------------------------------------
 // System and application specific headers
@@ -10,11 +10,29 @@
 
 int main( void ) {
 	// Load data
+	int op, id;
 	initialGuide();
 	Data_t *data = start_training();
-	createGraph();
+	//createGraph();
+
+	op = SuggestionsOptions();
+	if(op==1) 
+    {
+        id = movieSuggestionUser(data); 
+		printf("Deberias ver ->   %s\n", data->movies[id]->name);
+    }
+	if(op==2) 
+    {
+        id = friendsSuggestions(data); 
+		printf("Deberias conocer a ->   %s\n", data->users[id]->name);
+    }
+	if(op==3) 
+    {
+        id = movieSuggestionMovie(data); 
+		printf("Te podria gustar ->   %s\n", data->movies[id]->name);
+    }
 
 	// Print report
-	print_in_file(REPORTS_FILE, data);
+	//print_in_file(REPORTS_FILE, data);
 	return 0;
 }
